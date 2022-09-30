1682e1ca0b18a4c349e6a1c336328085d60d57b3
gooby <61945370+celojevic@users.noreply.github.com>
2022-09-30 13:30:46 -0400

    NPS full state sync on client join option

--- Assets/NetworkParticleSystem/Scripts/NetworkParticleSystem.cs
+++ Assets/NetworkParticleSystem/Scripts/NetworkParticleSystem.cs
@@ -1,4 +1,5 @@
 using FishNet.Connection;
+using FishNet.Managing.Logging;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using System.Collections.Generic;
@@ -8,6 +9,10 @@ using static UnityEngine.ParticleSystem;
 
 namespace gooby.NetworkParticleSystem
 {
+
+    /// <summary>
+    /// Syncs a Unity ParticleSystem over the network.
+    /// </summary>
     [RequireComponent(typeof(ParticleSystem))]
     public class NetworkParticleSystem : NetworkBehaviour
     {
@@ -22,24 +27,40 @@ namespace gooby.NetworkParticleSystem
             public override string ToString() => $"Seed: {Seed}, PS Time: {Time}, Server tick: {Tick}";
         }
 
-        [SerializeField] private bool _syncSeed = true;
+        [Tooltip("True to sync particle system autoplay data over the network " +
+            "so it appears the same to clients as it does on the server.")]
+        [SerializeField] 
+        private bool _syncSeed = true;
 
+        [Tooltip("True to sync the full particle state to clients when they join.")]
+        [SerializeField] 
+        private bool _syncFullStateOnJoin = false;
+
+        [Tooltip("The sibling particle system component to sync over the network.")]
         [SerializeField] private ParticleSystem _particleSystem = null;
         public ParticleSystem ParticleSystem => _particleSystem;
 
         [Header("Emission Sync")]
+        [Tooltip("True to sync the birth and death of particles over the network. " +
+            "You probably want this on unless you are handling collisions on the client.")]
         [SerializeField] private bool _syncAllParticles = false;
         private int _particleCount;
 
+        /// <summary>
+        /// Particle cache.
+        /// </summary>
         private Particle[] _particles;
         public Particle[] Particles => _particles;
 
+        /// <summary>
+        /// Particle system custom data cache.
+        /// </summary>
         private List<Vector4> _customData = new List<Vector4>();
         public List<Vector4> CustomData => _customData;
 
-        [SyncVar(OnChange = nameof(OnSeedChanged))]
+        [SyncVar(OnChange = nameof(OnSyncDataChanged))]
         private ParticleSyncData _psSyncData;
-        private void OnSeedChanged(ParticleSyncData prev, ParticleSyncData cur, bool asServer)
+        private void OnSyncDataChanged(ParticleSyncData prev, ParticleSyncData cur, bool asServer)
         {
             if (asServer || IsHost) return;
 
@@ -50,28 +71,72 @@ namespace gooby.NetworkParticleSystem
         }
 
         [Header("Events")]
-        /// <summary>
-        /// Invokes the GameObject that collided with the particle.
-        /// </summary>
+        [Tooltip("Invokes the GameObject that collided with the particle.")]
         public UnityEvent<GameObject> OnParticleCollisionWithGameObject;
-        /// <summary>
-        /// Invokes the index in the particles array of the particle that was born.
-        /// </summary>
+        [Tooltip("Invokes the index in the particles array of the particle that was born.")]
         public UnityEvent<int> OnParticleBirth;
-        /// <summary>
-        /// Invokes the index in the particles array of the particle that died.
-        /// </summary>
+        [Tooltip("Invokes the index in the particles array of the particle that died.")]
         public UnityEvent<int> OnParticleDeath;
 
+        [Header("Debug")]
+        [Tooltip("Debug log level.")]
+        [SerializeField] private LoggingType _logLevel = LoggingType.Error;
+
+        private void Awake()
+        {
+            _particles = new Particle[_particleSystem.main.maxParticles];
+        }
+
+        private void LateUpdate()
+        {
+            if (IsServer && _syncAllParticles)
+            {
+                GetParticleData();
+
+                if (_particleCount > 0)
+                {
+                    // very spammy
+                    //Debug.Log($"Particle count: {_particleCount}");
+
+                    for (int i = 0; i < _particleCount; i++)
+                    {
+                        // particle born
+                        if (_customData[i].x == 0)
+                        {
+                            int id = GetUniqueID();
+                            _customData[i] = new Vector4(id, 0, 0, 0);
+
+                            if (_logLevel >= LoggingType.Common)
+                                Debug.Log($"Particle index {i} NEW ID: {_customData[i].x}");
+
+#pragma warning disable 0618
+                            RpcEmit(_particles[i].position, _particles[i].velocity,
+                                _particles[i].size, _particles[i].remainingLifetime, _particles[i].color,
+                                id);
+#pragma warning restore 0618
+
+                            OnParticleBirth?.Invoke(i);
+                        }
+                        else if (_particles[i].remainingLifetime <= 0f)
+                        {
+                            OnParticleDeath?.Invoke(i);
+
+                            if (_logLevel >= LoggingType.Common)
+                                Debug.Log($"Particle DEAD, index {i} ID: {_customData[i].x}");
+
+                            RpcKill((int)_customData[i].x);
+                            _customData[i] = Vector4.zero;
+                        }
+                    }
+
+                    SetParticleData();
+                }
+            }
+        }
 
         private void OnParticleCollision(GameObject other)
         {
             InvokeParticleCollision(other);
-        }
-
-        void InvokeParticleCollision(GameObject other)
-        {
-            OnParticleCollisionWithGameObject?.Invoke(other);
         }
 
         public override void OnSpawnServer(NetworkConnection connection)
@@ -87,54 +152,68 @@ namespace gooby.NetworkParticleSystem
                     Tick = TimeManager.Tick,
                 };
             }
-        }
 
-        float OneWayRTT(uint serverTick) => (float)TimeManager.TicksToTime(TimeManager.Tick - serverTick);
-
-        private void Awake()
-        {
-            _particles = new Particle[_particleSystem.main.maxParticles];
-        }
-
-        private void LateUpdate()
-        {
-            if (IsServer && _syncAllParticles)
+            if (_syncFullStateOnJoin)
             {
-                GetParticleData();
+                SendFullState(connection);
+            }
+        }
 
-                if (_particleCount > 0)
-                {
-                    //Debug.Log($"Particle count: {_particleCount}");
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
 
-                    for (int i = 0; i < _particleCount; i++)
-                    {
-                        if (_customData[i].x == 0)
-                        {
-                            int id = GetUniqueID();
-                            // particle born
-                            _customData[i] = new Vector4(id, 0, 0, 0);
-                            //Debug.Log($"Particle index {i} NEW ID: {_customData[i].x}");
+            // host check
+            if (IsServer) return;
 
+            // disable autoplay on clients, they will play when they get the data
 #pragma warning disable 0618
-                            RpcEmit(_particles[i].position, _particles[i].velocity,
-                                _particles[i].size, _particles[i].remainingLifetime, _particles[i].color,
-                                id);
+            _particleSystem.playOnAwake = false;
 #pragma warning restore 0618
 
-                            OnParticleBirth?.Invoke(i);
-                        }
-                        else if (_particles[i].remainingLifetime <= 0f)
-                        {
-                            OnParticleDeath?.Invoke(i);
+            // disable collision on clients.
+            // the server will handle it and send RpcKill's for client to remove them
+            var col = _particleSystem.collision;
+            col.enabled = false;
+        }
 
-                            //Debug.Log($"Particle DEAD, index {i} ID: {_customData[i].x}");
-                            RpcKill((int)_customData[i].x);
-                            _customData[i] = Vector4.zero;
-                        }
-                    }
+        /// <summary>
+        /// Invokes particle collision events.
+        /// </summary>
+        /// <param name="other"></param>
+        private void InvokeParticleCollision(GameObject other)
+        {
+            OnParticleCollisionWithGameObject?.Invoke(other);
+        }
 
-                    SetParticleData();
-                }
+        private float OneWayRTT(uint serverTick)
+        {
+            return (float)TimeManager.TicksToTime(TimeManager.Tick - serverTick);
+        }
+
+        /// <summary>
+        /// Emits all the existing particles on the client of the given conn.
+        /// </summary>
+        /// <param name="conn"></param>
+        private void SendFullState(NetworkConnection conn)
+        {
+            if (_logLevel >= LoggingType.Common)
+                Debug.Log("Sending full state");
+
+            GetParticleData();
+
+            if (_particleCount == 0) return;
+
+            for (int i = 0; i < _particleCount; i++)
+            {
+                // skip dead particles
+                if (_particles[i].remainingLifetime <= 0f) continue;
+
+#pragma warning disable 0618
+                TargetEmit(conn, _particles[i].position, _particles[i].velocity,
+                    _particles[i].size, _particles[i].remainingLifetime, _particles[i].color,
+                    (int)_customData[i].x);
+#pragma warning restore 0618
             }
         }
 
@@ -162,22 +241,20 @@ namespace gooby.NetworkParticleSystem
             SetParticleData();
         }
 
-        public override void OnStartClient()
-        {
-            base.OnStartClient();
-
-            // host check
-            if (IsServer) return;
-
-            // disable collision on clients.
-            // the server will handle it and send RpcKill's for client to remove them
-            var col = _particleSystem.collision;
-            col.enabled = false;
-        }
-
         [ObserversRpc]
-        void RpcEmit(Vector3 pos, Vector3 vel, float size, float lifetime, Color32 color,
-            int id)
+        void RpcEmit(Vector3 pos, Vector3 vel, float size, float lifetime, Color32 color, int id)
+        {
+            Emit(pos, vel, size, lifetime, color, id);
+        }
+        [TargetRpc]
+        void TargetEmit(NetworkConnection conn, Vector3 pos, Vector3 vel, float size, float lifetime, Color32 color, int id)
+        {
+            if (_logLevel >= LoggingType.Common)
+                Debug.Log("Emitting at " + pos);
+
+            Emit(pos, vel, size, lifetime, color, id);
+        }
+        void Emit(Vector3 pos, Vector3 vel, float size, float lifetime, Color32 color, int id)
         {
             if (IsServer) return;
 
@@ -190,11 +267,16 @@ namespace gooby.NetworkParticleSystem
 
             for (int i = 0; i < _particleCount; i++)
             {
-                //Debug.Log($"Particle {i} empty id");
+                if (_logLevel >= LoggingType.Common)
+                    Debug.Log($"Particle {i} empty id");
+
                 if (_customData[i].x == 0)
                 {
                     _customData[i] = new Vector4(id, 0, 0, 0);
-                    //Debug.Log($"Particle index {i} NEW ID: {_customData[i].x}");
+
+                    if (_logLevel >= LoggingType.Common)
+                        Debug.Log($"Particle index {i} NEW ID: {_customData[i].x}");
+
                     OnParticleBirth?.Invoke(i);
                 }
             }
